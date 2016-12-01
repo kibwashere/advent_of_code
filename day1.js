62adcece9ba361fa868e364b392c0b4275b8dfce
Voldemortas <eragonas5@gmail.com>
2016-12-01 10:53:06 +0200

    Why you no work? :(

---
+++ day1.js
@@ -0,0 +1,22 @@
+'use strict';
+const input = 'R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3';
+const arr = input.split(', ');
+let direction = 0;//0 - N, 1 - E, 2 - S, 3 - W
+let position = [0, 0];//x & y
+function move(txt){
+    if(txt[0] == 'R'){
+        direction++;
+    }else{
+        direction--;
+    }
+    direction = Math.abs(direction%4);
+    txt = txt.substring(1) * 1;
+    if(direction >= 2){
+        txt *= -1;
+    }
+    position[(direction+1)%2] += txt;
+}
+for(let i = 0; i < arr.length; i++){
+    move(arr[i]);
+}
+console.log(Math.abs(position[0]) + Math.abs(position[1]));//[240, -139]
